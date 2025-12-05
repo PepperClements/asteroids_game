436388028deb53d398949b95ea31b459736ead6f
PepperClements <santiagojara11@gmail.com>
2025-12-04 19:26:19 -0500

    B: added screen and sprite

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 
 def draw_screen():
+    pygame.time.Clock 
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +18,11 @@ def draw_screen():
                 return
         screen.fill("black") 
         display.flip()
+        pygame.time.Clock().tick(60) 
+        dt=pygame.time.Clock().tick(60) / 1000
+        
+
+
 
 draw_screen()
 
@@ -24,6 +32,7 @@ def main():
     print("Starting Asteroids with pygame version: ", pygame.__version__)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
 
 if __name__ == "__main__":
