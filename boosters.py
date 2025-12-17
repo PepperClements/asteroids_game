70dd14b3309c0efc8666c12d5b7117c9b1a6cc75
PepperClements <santiagojara11@gmail.com>
2025-12-16 21:39:26 -0500

    I

--- boosters.py
+++ boosters.py
@@ -3,12 +3,12 @@ from circleshape import CircleShape
 from constants import *
 
 
-class booster (CircleShape):
+class Booster (CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
     def draw(self, screen):
-        pygame.draw.square(screen, "blue",self.position,self.radius, LINE_WIDTH)
+        pygame.draw.circle(screen, "blue",self.position,self.radius, LINE_WIDTH)
     def update(self, dt):
          self.position += (self.velocity * dt)
     def boost(self,player):
-        player.velocity += self.velocity
+        player.velocity *= 2
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from asteroidfield import AsteroidField
 from logger import log_event
 from shot import Shot
 from score import display_score, update_score
-
+from boosters import Booster
 
 
 def main():
@@ -19,6 +19,8 @@ def main():
     drawable=pygame.sprite.Group()
     asteroids=pygame.sprite.Group()
     shots=pygame.sprite.Group()
+    boosters = pygame.sprite.Group()
+    Booster.containers = (updatable,drawable,boosters)
     AsteroidField.containers = (updatable)
     asteroid_field = AsteroidField()
     Shot.containers = (updatable,drawable,shots)
@@ -31,7 +33,7 @@ def main():
 
     while True:
         log_state()
-        
+        booster = Booster(400, 300, 15)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -52,6 +54,14 @@ def main():
                     score= update_score(score,10)
                     log_event("asteroid_shot")
         screen.fill("black")
+        
+        for booster in boosters:
+            booster.update(dt)
+            booster.draw(screen)
+            if player.collides_with(booster):
+                booster.boost(player)
+                log_event("player_boosted")
+                booster.kill()
 
         for d in drawable:
             d.draw(screen)
@@ -61,7 +71,7 @@ def main():
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
+        
 
 if __name__ == "__main__":
     main()
